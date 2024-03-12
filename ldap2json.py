b7ac7781818dc72ddc93f69dfe138377c0e2a627
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2024-03-12 19:53:32 +0100

    Moved to sectools

--- ldap2json.py
+++ ldap2json.py
@@ -4,21 +4,12 @@
 # Author             : Podalirius (@podalirius_)
 # Date created       : 22 Dec 2021
 
-from binascii import unhexlify
-from impacket.smbconnection import SMBConnection, SMB2_DIALECT_002, SMB2_DIALECT_21, SMB_DIALECT, SessionError
-from impacket.spnego import SPNEGO_NegTokenInit, TypesMech
-from ldap3.protocol.formatters.formatters import format_sid
-from ldap3.protocol.microsoft import security_descriptor_control
+
+from sectools.windows.ldap import init_ldap_session, raw_ldap_query
 import argparse
 import datetime
 import json
-import ldap3
-import logging
-import os
-import ssl
 import sys
-
-### Data utils
 
 
 def cast_to_dict(cid):
@@ -49,290 +40,16 @@ def cast_to_dict(cid):
     return out
 
 
-def dict_get_paths(d):
-    paths = []
-    for key in d.keys():
-        if type(d[key]) == dict:
-            paths = [[key]+p for p in dict_get_paths(d[key])]
-        else:
-            paths.append([key])
-    return paths
-
-
-def dict_path_access(d, path):
-    for key in path:
-        if key in d.keys():
-            d = d[key]
-        else:
-            return None
-    return d
-
-
-### LDAPConsole
-
-class LDAPConsole(object):
-    """docstring for LDAPConsole."""
-
-    def __init__(self, ldap_server, ldap_session, target_dn, debug=False):
-        super(LDAPConsole, self).__init__()
-        self.ldap_server = ldap_server
-        self.ldap_session = ldap_session
-        self.delegate_from = None
-        self.target_dn = target_dn
-        self.debug = debug
-
-    def query(self, query, attributes=['*']):
-        results = {}
-        try:
-            # https://ldap3.readthedocs.io/en/latest/searches.html#the-search-operation
-            paged_response = True
-            paged_cookie = None
-            controls = security_descriptor_control(sdflags=0x04)
-            while paged_response:
-                self.ldap_session.search(
-                    self.target_dn, query, attributes=attributes,
-                    size_limit=0, paged_size=1000, paged_cookie=paged_cookie, controls=controls
-                )
-                if "controls" in self.ldap_session.result.keys():
-                    if "1.2.840.113556.1.4.319" in self.ldap_session.result["controls"].keys():
-                        _tmp_cookie = self.ldap_session.result["controls"]["1.2.840.113556.1.4.319"]["value"]["cookie"]
-                        if len(_tmp_cookie) == 0:
-                            paged_response = False
-                        else:
-                            paged_response = True
-                            paged_cookie = _tmp_cookie
-                    else:
-                        paged_response = False
-                else:
-                    paged_response = False
-                #
-                for entry in self.ldap_session.response:
-                    if entry['type'] != 'searchResEntry':
-                        continue
-                    results[entry['dn']] = entry["attributes"]
-        except ldap3.core.exceptions.LDAPInvalidFilterError as e:
-            print("Invalid Filter. (ldap3.core.exceptions.LDAPInvalidFilterError)")
-        except Exception as e:
-            raise e
-        return results
-
-def get_machine_name(args, domain):
-    if args.dc_ip is not None:
-        s = SMBConnection(args.dc_ip, args.dc_ip)
-    else:
-        s = SMBConnection(domain, domain)
-    try:
-        s.login('', '')
-    except Exception:
-        if s.getServerName() == '':
-            raise Exception('Error while anonymous logging into %s' % domain)
-    else:
-        s.logoff()
-    return s.getServerName()
-
-
-def init_ldap_connection(target, tls_version, args, domain, username, password, lmhash, nthash):
-    user = '%s\\%s' % (domain, username)
-    if tls_version is not None:
-        use_ssl = True
-        port = 636
-        tls = ldap3.Tls(validate=ssl.CERT_NONE, version=tls_version)
-    else:
-        use_ssl = False
-        port = 389
-        tls = None
-    ldap_server = ldap3.Server(target, get_info=ldap3.ALL, port=port, use_ssl=use_ssl, tls=tls)
-
-    if args.use_kerberos:
-        ldap_session = ldap3.Connection(ldap_server)
-        ldap_session.bind()
-        ldap3_kerberos_login(ldap_session, target, username, password, domain, lmhash, nthash, args.auth_key, kdcHost=args.dc_ip)
-    elif args.auth_hashes is not None:
-        if lmhash == "":
-            lmhash = "aad3b435b51404eeaad3b435b51404ee"
-        ldap_session = ldap3.Connection(ldap_server, user=user, password=lmhash+":"+nthash, authentication=ldap3.NTLM, auto_bind=True)
-    else:
-        ldap_session = ldap3.Connection(ldap_server, user=user, password=password, authentication=ldap3.NTLM, auto_bind=True)
-
-    return ldap_server, ldap_session
-
-
-def init_ldap_session(args, domain, username, password, lmhash, nthash):
-    if args.use_kerberos:
-        target = get_machine_name(args, domain)
-    else:
-        if args.dc_ip is not None:
-            target = args.dc_ip
-        else:
-            target = domain
-
-    if args.use_ldaps is True:
-        try:
-            return init_ldap_connection(target, ssl.PROTOCOL_TLSv1_2, args, domain, username, password, lmhash, nthash)
-        except ldap3.core.exceptions.LDAPSocketOpenError:
-            return init_ldap_connection(target, ssl.PROTOCOL_TLSv1, args, domain, username, password, lmhash, nthash)
-    else:
-        return init_ldap_connection(target, None, args, domain, username, password, lmhash, nthash)
-
-
-def ldap3_kerberos_login(connection, target, user, password, domain='', lmhash='', nthash='', aesKey='', kdcHost=None, TGT=None, TGS=None, useCache=True):
-    from pyasn1.codec.ber import encoder, decoder
-    from pyasn1.type.univ import noValue
-    """
-    logins into the target system explicitly using Kerberos. Hashes are used if RC4_HMAC is supported.
-    :param string user: username
-    :param string password: password for the user
-    :param string domain: domain where the account is valid for (required)
-    :param string lmhash: LMHASH used to authenticate using hashes (password is not used)
-    :param string nthash: NTHASH used to authenticate using hashes (password is not used)
-    :param string aesKey: aes256-cts-hmac-sha1-96 or aes128-cts-hmac-sha1-96 used for Kerberos authentication
-    :param string kdcHost: hostname or IP Address for the KDC. If None, the domain will be used (it needs to resolve tho)
-    :param struct TGT: If there's a TGT available, send the structure here and it will be used
-    :param struct TGS: same for TGS. See smb3.py for the format
-    :param bool useCache: whether or not we should use the ccache for credentials lookup. If TGT or TGS are specified this is False
-    :return: True, raises an Exception if error.
-    """
-
-    if lmhash != '' or nthash != '':
-        if len(lmhash) % 2:
-            lmhash = '0' + lmhash
-        if len(nthash) % 2:
-            nthash = '0' + nthash
-        try:  # just in case they were converted already
-            lmhash = unhexlify(lmhash)
-            nthash = unhexlify(nthash)
-        except TypeError:
-            pass
-
-    # Importing down here so pyasn1 is not required if kerberos is not used.
-    from impacket.krb5.ccache import CCache
-    from impacket.krb5.asn1 import AP_REQ, Authenticator, TGS_REP, seq_set
-    from impacket.krb5.kerberosv5 import getKerberosTGT, getKerberosTGS
-    from impacket.krb5 import constants
-    from impacket.krb5.types import Principal, KerberosTime, Ticket
-    import datetime
-
-    if TGT is not None or TGS is not None:
-        useCache = False
-
-    if useCache:
-        try:
-            ccache = CCache.loadFile(os.getenv('KRB5CCNAME'))
-        except Exception as e:
-            # No cache present
-            print(e)
-            pass
-        else:
-            # retrieve domain information from CCache file if needed
-            if domain == '':
-                domain = ccache.principal.realm['data'].decode('utf-8')
-                logging.debug('Domain retrieved from CCache: %s' % domain)
-
-            logging.debug('Using Kerberos Cache: %s' % os.getenv('KRB5CCNAME'))
-            principal = 'ldap/%s@%s' % (target.upper(), domain.upper())
-
-            creds = ccache.getCredential(principal)
-            if creds is None:
-                # Let's try for the TGT and go from there
-                principal = 'krbtgt/%s@%s' % (domain.upper(), domain.upper())
-                creds = ccache.getCredential(principal)
-                if creds is not None:
-                    TGT = creds.toTGT()
-                    logging.debug('Using TGT from cache')
-                else:
-                    logging.debug('No valid credentials found in cache')
-            else:
-                TGS = creds.toTGS(principal)
-                logging.debug('Using TGS from cache')
-
-            # retrieve user information from CCache file if needed
-            if user == '' and creds is not None:
-                user = creds['client'].prettyPrint().split(b'@')[0].decode('utf-8')
-                logging.debug('Username retrieved from CCache: %s' % user)
-            elif user == '' and len(ccache.principal.components) > 0:
-                user = ccache.principal.components[0]['data'].decode('utf-8')
-                logging.debug('Username retrieved from CCache: %s' % user)
-
-    # First of all, we need to get a TGT for the user
-    userName = Principal(user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
-    if TGT is None:
-        if TGS is None:
-            tgt, cipher, oldSessionKey, sessionKey = getKerberosTGT(userName, password, domain, lmhash, nthash, aesKey, kdcHost)
-    else:
-        tgt = TGT['KDC_REP']
-        cipher = TGT['cipher']
-        sessionKey = TGT['sessionKey']
-
-    if TGS is None:
-        serverName = Principal('ldap/%s' % target, type=constants.PrincipalNameType.NT_SRV_INST.value)
-        tgs, cipher, oldSessionKey, sessionKey = getKerberosTGS(serverName, domain, kdcHost, tgt, cipher, sessionKey)
-    else:
-        tgs = TGS['KDC_REP']
-        cipher = TGS['cipher']
-        sessionKey = TGS['sessionKey']
-
-        # Let's build a NegTokenInit with a Kerberos REQ_AP
-
-    blob = SPNEGO_NegTokenInit()
-
-    # Kerberos
-    blob['MechTypes'] = [TypesMech['MS KRB5 - Microsoft Kerberos 5']]
-
-    # Let's extract the ticket from the TGS
-    tgs = decoder.decode(tgs, asn1Spec=TGS_REP())[0]
-    ticket = Ticket()
-    ticket.from_asn1(tgs['ticket'])
-
-    # Now let's build the AP_REQ
-    apReq = AP_REQ()
-    apReq['pvno'] = 5
-    apReq['msg-type'] = int(constants.ApplicationTagNumbers.AP_REQ.value)
-
-    opts = []
-    apReq['ap-options'] = constants.encodeFlags(opts)
-    seq_set(apReq, 'ticket', ticket.to_asn1)
-
-    authenticator = Authenticator()
-    authenticator['authenticator-vno'] = 5
-    authenticator['crealm'] = domain
-    seq_set(authenticator, 'cname', userName.components_to_asn1)
-    now = datetime.datetime.utcnow()
-
-    authenticator['cusec'] = now.microsecond
-    authenticator['ctime'] = KerberosTime.to_asn1(now)
-
-    encodedAuthenticator = encoder.encode(authenticator)
-
-    # Key Usage 11
-    # AP-REQ Authenticator (includes application authenticator
-    # subkey), encrypted with the application session key
-    # (Section 5.5.1)
-    encryptedEncodedAuthenticator = cipher.encrypt(sessionKey, 11, encodedAuthenticator, None)
-
-    apReq['authenticator'] = noValue
-    apReq['authenticator']['etype'] = cipher.enctype
-    apReq['authenticator']['cipher'] = encryptedEncodedAuthenticator
-
-    blob['MechToken'] = encoder.encode(apReq)
-
-    request = ldap3.operation.bind.bind_operation(connection.version, ldap3.SASL, user, None, 'GSS-SPNEGO',
-                                                  blob.getData())
-
-    # Done with the Kerberos saga, now let's get into LDAP
-    if connection.closed:  # try to open connection if closed
-        connection.open(read_server_info=False)
-
-    connection.sasl_in_progress = True
-    response = connection.post_send_single_response(connection.send('bindRequest', request, None))
-    connection.sasl_in_progress = False
-    if response[0]['result'] != 0:
-        raise Exception(response)
-
-    connection.bound = True
-
-    return True
-
 def bytessize(data):
+    """
+    Function to calculate the size of data in bytes and convert it to a human-readable format.
+
+    Args:
+        data (bytes): The data for which the size needs to be calculated.
+
+    Returns:
+        str: The human-readable format of the data size.
+    """
     l = len(data)
     units = ['B','kB','MB','GB','TB','PB']
     for k in range(len(units)):
@@ -340,7 +57,8 @@ def bytessize(data):
             break
     return "%4.2f %s" % (round(l/(1024**(k)),2), units[k])
 
-def parse_args():
+
+def parseArgs():
     parser = argparse.ArgumentParser(add_help=True, description='The ldap2json script allows you to extract the whole LDAP content of a Windows domain into a JSON file.')
     parser.add_argument('--use-ldaps', action='store_true', help='Use LDAPS instead of LDAP')
     parser.add_argument("-q", "--quiet", dest="quiet", action="store_true", default=False, help="show no information at all")
@@ -365,21 +83,21 @@ def parse_args():
         parser.print_help()
         sys.exit(1)
         
-    args = parser.parse_args()
+    options = parser.parse_args()
     
-    if args.auth_password is None and args.no_pass == False and args.auth_hashes is None:
-        print("[+] No password of hashes provided and --no-pass is '%s'" % args.no_pass)
+    if options.auth_password is None and options.no_pass == False and options.auth_hashes is None:
+        print("[+] No password of hashes provided and --no-pass is '%s'" % options.no_pass)
         from getpass import getpass
-        if args.auth_domain is not None:
-            args.auth_password = getpass("  | Provide a password for '%s\\%s':" % (args.auth_domain, args.auth_username))
+        if options.auth_domain is not None:
+            options.auth_password = getpass("  | Provide a password for '%s\\%s':" % (options.auth_domain, options.auth_username))
         else:
-            args.auth_password = getpass("  | Provide a password for '%s':" % args.auth_username)
+            options.auth_password = getpass("  | Provide a password for '%s':" % options.auth_username)
 
-    return args
+    return options
 
 
 if __name__ == '__main__':
-    args = parse_args()
+    options = parseArgs()
 
     print("[+]======================================================")
     print("[+]    LDAP2JSON v1.1                  @podalirius_      ")
@@ -388,38 +106,59 @@ if __name__ == '__main__':
 
     auth_lm_hash = ""
     auth_nt_hash = ""
-    if args.auth_hashes is not None:
-        if ":" in args.auth_hashes:
-            auth_lm_hash = args.auth_hashes.split(":")[0]
-            auth_nt_hash = args.auth_hashes.split(":")[1]
+    if options.auth_hashes is not None:
+        if ":" in options.auth_hashes:
+            auth_lm_hash = options.auth_hashes.split(":")[0]
+            auth_nt_hash = options.auth_hashes.split(":")[1]
         else:
-            auth_nt_hash = args.auth_hashes
+            auth_nt_hash = options.auth_hashes
 
+    print("[>] Connecting to remote LDAP host '%s' ... " % options.dc_ip, end="", flush=True)
     ldap_server, ldap_session = init_ldap_session(
-        args=args,
-        domain=args.auth_domain,
-        username=args.auth_username,
-        password=args.auth_password,
-        lmhash=auth_lm_hash,
-        nthash=auth_nt_hash
+        auth_domain=options.auth_domain, 
+        auth_username=options.auth_username, 
+        auth_password=options.auth_password, 
+        auth_lm_hash=auth_lm_hash, 
+        auth_nt_hash=auth_nt_hash, 
+        auth_key=options.auth_key, 
+        use_kerberos=options.use_kerberos, 
+        kdcHost=options.kdcHost, 
+        use_ldaps=options.use_ldaps, 
+        auth_dc_ip=options.dc_ip
     )
+    configurationNamingContext = ldap_server.info.other["configurationNamingContext"]
+    defaultNamingContext = ldap_server.info.other["defaultNamingContext"]
+    print("done.")
 
-    if args.debug:
+    if options.debug:
         print("[>] Authentication successful!")
 
     print("[>] Extracting all objects from LDAP ...")
     
     data = {}
     for naming_context in ldap_server.info.naming_contexts:
-        if args.debug:
+        if options.debug:
             print("[>] Querying (objectClass=*) to LDAP on %s ..." % naming_context)
 
-        lc = LDAPConsole(ldap_server, ldap_session, naming_context, debug=args.debug)
-        response = lc.query("(objectClass=*)", attributes=["*"])
-        if args.debug:
+        response = raw_ldap_query(
+            auth_domain=options.auth_domain,
+            auth_dc_ip=options.dc_ip,
+            auth_username=options.auth_username,
+            auth_password=options.auth_password,
+            auth_hashes=options.auth_hashes,
+            auth_key=options.auth_key,
+            searchbase=naming_context,
+            use_ldaps=options.use_ldaps,
+            use_kerberos=options.use_kerberos,
+            kdcHost=options.kdcHost,
+            query="(objectClass=*)",
+            attributes=["*"]
+        )
+
+        if options.debug:
             print("[>] LDAP query (objectClass=*) returned %d objects" % len(response))
 
-        if args.debug:
+        if options.debug:
             print("[>] Parsing data ...")
 
         for cn in response:
@@ -434,11 +173,11 @@ if __name__ == '__main__':
             tmp[path[-1]] = cast_to_dict(response[cn])
 
     json_data = json.dumps(data, indent=4)
-    if args.debug:
+    if options.debug:
         print("[>] JSON data generated.")
 
-    print("[>] Writing json data to %s" % args.jsonfile)
-    f = open(args.jsonfile, 'w')
+    print("[>] Writing json data to %s" % options.jsonfile)
+    f = open(options.jsonfile, 'w')
     f.write(json_data)
     f.close()
-    print("[>] Written %s bytes to %s" % (bytessize(json_data), args.jsonfile))
+    print("[>] Written %s bytes to %s" % (bytessize(json_data), options.jsonfile))
